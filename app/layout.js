41708f498a78fa4465d34205c0ded155d230a226
Lalo (Edward) <67384140+lalohamb@users.noreply.github.com>
2024-12-16 22:26:29 -0600

    Initial commit

---
+++ app/layout.js
@@ -0,0 +1,27 @@
+import './globals.css'
+import { Josefin_Sans } from 'next/font/google'
+
+const josefin_sans = Josefin_Sans({ subsets: ['latin'] })
+
+// Components
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
+export const metadata = {
+  title: 'CoinDragon',
+  description: 'Your personal crypto portfolio',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={josefin_sans.className}>
+        <Navbar />
+
+        {children}
+
+        <Footer />
+      </body>
+    </html>
+  )
+}
